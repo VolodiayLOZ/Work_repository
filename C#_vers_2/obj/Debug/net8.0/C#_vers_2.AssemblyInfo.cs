e8fcb7ee0b124f52a7ee8ae889d4d00721731f3a
Vova <dgocker2006@yandex.ru>
2024-04-06 14:38:58 +0400

    *, /

--- C#_vers_2/obj/Debug/net8.0/C#_vers_2.AssemblyInfo.cs
+++ C#_vers_2/obj/Debug/net8.0/C#_vers_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_vers_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2cf648a68c8cdffd616f758a8da9ef4105358e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10b14cec223e03b3e5a5a08e3db2aa146eff84e")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_vers_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_vers_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
