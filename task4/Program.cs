88ec28bba1a1c99236cb33b28219c645ec62c38a
Vova <dgocker2006@yandex.ru>
2024-04-06 17:03:54 +0400

    max

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 6;
+int b = 12;
+int c = 333;
+int d = 1;
+int e = 111;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
